c8400ac64c140fd5aa6943ed829826b39cbb0051
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-03-09 17:38:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const allArguments = process.argv;
+const log = console.log;
+
+// log(allArguments[0]);
+// log(allArguments[1]);
+// log(allArguments[2]);
+log(allArguments.length);
+
+log(process.env.PORT);
+log(process.env.ABC);
